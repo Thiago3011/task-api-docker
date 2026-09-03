f2e6360a8953aff8cb891a09c5c8a8006301e8a8
Thiago3011 <thiago.silva1001@outlook.com>
2026-09-03 18:40:17 -0300

    feat: configure environment variables

--- app/connection/database.py
+++ app/connection/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./data/task_control.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
